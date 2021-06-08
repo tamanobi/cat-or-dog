a9582f47ef6cb03f7a2294972731ac93d075a7d5
tamanobi <tamanobi@gmail.com>
2021-06-09 03:36:28 +0900

    calc accuracy by class

--- main.py
+++ main.py
@@ -148,3 +148,25 @@ with torch.no_grad():
 print(
     "Accuracy of the network on the 10000 test images: %d %%" % (100 * correct / total)
 )
+
+# prepare to count predictions for each class
+correct_pred = {classname: 0 for classname in classes}
+total_pred = {classname: 0 for classname in classes}
+
+# again no gradients needed
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predictions = torch.max(outputs, 1)
+        # collect the correct predictions for each class
+        for label, prediction in zip(labels, predictions):
+            if label == prediction:
+                correct_pred[classes[label]] += 1
+            total_pred[classes[label]] += 1
+
+
+# print accuracy for each class
+for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count) / total_pred[classname]
+    print("Accuracy for class {:5s} is: {:.1f} %".format(classname, accuracy))
