8f40999f112959f1ec81ea0401123c033eb38628
tamanobi <tamanobi@gmail.com>
2021-06-09 03:29:48 +0900

    calc accuracy

--- main.py
+++ main.py
@@ -131,3 +131,20 @@ outputs = net(images)
 _, predicted = torch.max(outputs, 1)
 
 print("Predicted: ", " ".join("%5s" % classes[predicted[j]] for j in range(4)))
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(
+    "Accuracy of the network on the 10000 test images: %d %%" % (100 * correct / total)
+)
