9dc92c5f5380aa007aa956a60307dc45e5c9ef42
tamanobi <tamanobi@gmail.com>
2021-06-09 03:24:35 +0900

    save models

--- main.py
+++ main.py
@@ -112,3 +112,6 @@ for epoch in range(2):  # loop over the dataset multiple times
             running_loss = 0.0
 
 print("Finished Training")
+
+PATH = "./cifar_net.pth"
+torch.save(net.state_dict(), PATH)
